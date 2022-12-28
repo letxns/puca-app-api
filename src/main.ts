60390a2d657e2f17c59c7d5133182367a1db00b3
letxns <lele_martins1999@yahoo.com.br>
2022-12-28 01:33:43 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  
+  //permitindo acesso geral para teste
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('api');
+
 
   await app.listen(3000);
 }
